c433bb2edfc338b8fe368a55e45b43b72c1dd39c
Nikita Klimenko <germismc@gmail.com>
2017-03-03 23:35:31 +0300

    Fork changes: readme, configs, sql queries.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+FORK settings
+======
+Please read the original readme and follow the steps for building. All configuration files are already configured.
+
 APIs
 ======
 The apis (APIs Secure) project offers an OAuth 2.0 Authorization Server that can be used to kickstart your API authentication. In essence it enables you to focus on your actual resource endpoints and use the out-of-the-box authorization server to authenticate resource owners and subsequently validate the access tokens that were granted to the Client applications. We will describe the typical use cases in more details in sections below.
--- apis-authorization-server/src/test/resources/db/migration/mysql/V1__auth-server-admin.sql
+++ apis-authorization-server/src/test/resources/db/migration/mysql/V1__auth-server-admin.sql
@@ -7,6 +7,13 @@ VALUES
 	'authorization-server-admin', 'cafebabe-cafe-babe-cafe-babecafebabe', null, 'https://static.surfconext.nl/media/logo-surfnet-small.png');
 INSERT INTO Resourceserver_scopes values (99998, 'read'),(99998, 'write') ;
 
+INSERT INTO resourceserver (id, contactEmail,  contactName, name, key0, secret, owner)
+VALUES
+	(101, 'admin@youdrive.com', 'you drive', 'main resource server', '174f3aac-0dc8-49e7-bffc-cc6f178427b1', '2cbec3bb-8837-49bd-bc0f-d35ca29505c1',
+	 'authorization-server-admin');
+
+INSERT INTO Resourceserver_scopes values (99998, 'read'),(99998, 'write') ;
+
 INSERT INTO client (id, contactEmail, contactName, description, name, thumbNailUrl, resourceserver_id,
 clientId, secret)
 VALUES
@@ -15,3 +22,7 @@ VALUES
     'https://static.surfconext.nl/media/logo-surfnet-small.png', 99998,
     'authorization-server-admin-js-client', '');
 INSERT INTO Client_scopes values (99998, 'read'), (99998, 'write');
+INSERT INTO accesstoken (id, creationDate, modificationDate, encodedPrincipal, expires, refreshToken, resourceOwnerId, token, client_id)
+VALUES
+  (51, '2017-02-11 17:45:54', '2017-02-11 17:45:54', '["org.surfnet.oaaas.conext.SAMLAuthenticatedPrincipal",{"name":"authorization-server-admin","roles":["java.util.ArrayList",[]],"groups":["java.util.ArrayList",[]],"adminPrincipal":false,"attributes":["java.util.HashMap",{"IDENTITY_PROVIDER":"http://mock-idp","DISPLAY_NAME":"authorization-server-admin"}]}]',
+  0, NULL, 'authorization-server-admin', 'b441fe73-deb2-4b88-ba35-a7828693ba59', '99998');
